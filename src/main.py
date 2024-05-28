3b5c0bf787218d715aa15fdf91b0384064199418
Rodrigo Martín Núñez <83213317+P-ict0@users.noreply.github.com>
2024-05-28 13:40:29 +0200

    Added logger

--- src/main.py
+++ src/main.py
@@ -1,2 +1,18 @@
+from src.utils.logger import Logger
+
+
+class Main:
+    def __init__(self) -> None:
+        self.logger = Logger(__name__).get_logger()
+
+    def test_logger(self) -> None:
+        self.logger.debug("This is a debug message.")
+        self.logger.info("This is an info message.")
+        self.logger.warning("This is a warning message.")
+        self.logger.error("This is an error message.")
+        self.logger.critical("This is a critical message.")
+
+
 if __name__ == "__main__":
-    pass
+    main = Main()
+    main.test_logger()
---
+++ src/utils/logger.py
@@ -0,0 +1,49 @@
+import logging
+from colorlog import ColoredFormatter
+
+
+class Logger:
+    """ A simple console logger class that outputs colorized logs. """
+
+    def __init__(self, name: str) -> None:
+        """
+        Initialize a colorized console logger.
+
+        Parameters:
+        name (str): Name of the logger which is typically the name of the module creating the logger.
+        """
+        # Create a logger
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)  # Set the threshold of logger to debug level
+
+        # Create console handler and set level to debug
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+
+        # Create colored formatter
+        formatter = ColoredFormatter(
+            "%(log_color)s[%(levelname)s]%(reset)s %(white)s%(message)s",
+            datefmt=None,
+            reset=True,
+            log_colors={
+                'DEBUG': 'cyan',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'red,bg_white',
+            },
+            secondary_log_colors={},
+            style='%'
+        )
+
+        # Add formatter to console handler
+        ch.setFormatter(formatter)
+
+        # Add ch to logger
+        self.logger.addHandler(ch)
+
+    def get_logger(self) -> logging.Logger:
+        """
+        Returns the configured logger with colorized output.
+        """
+        return self.logger
